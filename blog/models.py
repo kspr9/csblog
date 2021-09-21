5247aee961d6d1cbce1a8720bb16016c86e3691b
kspr9 <kaspar.pae@gmail.com>
2021-09-21 20:59:23 +0300

    created DB model and first entries + migrated

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+#for date_posted
+from django.utils import timezone
+#importing users to this
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now) # auto_now=True, when need to change time, auto_now_add = when post was created 
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # on_delete == when user is deleted, then also post is deleted
+
+    def __str__(self):
+        return self.title
