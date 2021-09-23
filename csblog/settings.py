5f1e2a7687ce832f7293696f28e43d24a97aca15
kspr9 <kaspar.pae@gmail.com>
2021-09-23 22:17:14 +0300

    Added user registering, connected all urls + added login template

--- csblog/settings.py
+++ csblog/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
+    'crispy_forms', # installed pip install django.crispy-forms
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4' # select which pack crispy_forms uses
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -125,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
--- users/views.py
+++ users/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
-def register(request): # views functions take always request as argument
+def register(request): # views.py functions take always 'request' as argument
     #django already has user registration form
-    form = UserCreationForm()
-    return render(request, 'users/register.html', {'form': form}) # renders first argument always is request
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save() #hashes password, and saves the form data to User model in django.contrib.auth.models
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('blog-home')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form}) # render's first argument always is request
+
+
