fac2f8829e1a30529eb10e69b375ed176b162ab7
kspr9 <kaspar.pae@gmail.com>
2021-09-29 06:07:07 +0300

    implemeted class based views, ordered posts by date, made post-detail view, made post links work

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import Post_ListView, Post_DetailView
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', Post_ListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', Post_DetailView.as_view(), name='post-detail'), # pk as primary key
     path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from .models import Post
+#importing list views for class based views
+from django.views.generic import ListView, DetailView
 
-def home(request):
-    context = {
-        'posts': Post.objects.all() # was posts referring to static posts list variable above
-    }
-    return render(request, 'blog/home.html', context)
+
+# Here defining all the views that are then configured to be shown in urls.py in urlpatterns
+
+# view used before adding class Post_ListView, was used in urlpatterns.py "path('', views.home, name='blog-home'),
+#def home(request):
+#    context = {
+#        'posts': Post.objects.all() # was posts referring to static posts list variable commented out below, now dynamic getting all Post's objects
+#    }
+#    return render(request, 'blog/home.html', context)
+
+class Post_ListView(ListView):
+    model = Post # 'Post' defined in models.py
+    template_name = 'blog/home.html' # default url is <app>/<model>_<viewtype>.html << change here to custom
+    context_object_name = 'posts' # home.html loops over 'for post in posts', this line here gives current context object (ie model = Post) a name  = current model ie Post model
+    ordering = ['-date_posted'] # orders posts from newest to oldest
+
+class Post_DetailView(DetailView):
+    model = Post # 'Post' defined in models.py
+
+    # looking for a template 
+    # 'blog/post_detail'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
